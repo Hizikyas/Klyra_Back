32285c443ff7f7efb50500a629fb1a9171603b57
Hizikyas Tamiru <hizikyastamiru@gmail.com>
2025-10-13 22:16:59 +0300

    restore the AuthenticationController

--- Controller/AuthenticationController.js
+++ Controller/AuthenticationController.js
@@ -136,8 +136,10 @@ exports.login = async (req , res , next) => {
     const cookieOptions = {
       expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
       httpOnly: true ,
-      secure : true ,
+      secure : false ,
       sameSite : 'none'
+      // secure : process.env.NODE_ENV === 'PRODUCTION' ,
+      // sameSite : process.env.NODE_ENV === 'PRODUCTION' ? 'none' : 'lax'
 
     }
     res.cookie('jwt', token, cookieOptions);
