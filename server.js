54ebe6725a814585fea5c0a733a3cae752ee5cc1
Hizikyas Tamiru <hizikyastamiru@gmail.com>
2025-11-06 22:12:00 +0300

    adding auth in the socket created

--- server.js
+++ server.js
@@ -32,9 +32,24 @@ app.set('io', io); // to set io instance in app for access in routes
 
 io.on('connection', (socket) => { // socket is the client(users browser or phone) and the io is main server that manages all the connections
   console.log('Socket connected:', socket.id);
+  const connectedUsers = new Map();
+  const userId = socket.handshake.auth?.userId || socket.handshake.query?.userId;
+
+  if (userId) {
+    connectedUsers.set(socket.id, userId);
+    socket.join(userId);
+
+    // Emit to ALL clients: this user is online
+    io.emit('userOnline', { userId });
+    console.log(`User ${userId} is online`);
+  }
 
   socket.on('joinUser', (userId) => { // this will add the user in the room , this code will listen to the event when users login and sends this code by sending userId socket.emit("joinUser" , userId) join the room with their userId, the room is created by their userId so if the message is sent by the room name so that the room contains the user which joins the room 
     socket.join(userId);
+    if (!connectedUsers.has(socket.id)) {
+      connectedUsers.set(socket.id, userId);
+      io.emit('userOnline', { userId });
+    }
   });
 
   socket.on('joinGroup', (groupId) => {
@@ -43,6 +58,17 @@ io.on('connection', (socket) => { // socket is the client(users browser or phone
 
   socket.on('disconnect', () => {
     console.log(`Socket ${socket.id} disconnected`);
+    const disconnectedUserId = connectedUsers.get(socket.id);
+    if (disconnectedUserId) {
+      connectedUsers.delete(socket.id);
+
+      // Emit to ALL clients: user went offline + lastSeen
+      io.emit('userOffline', {
+        userId: disconnectedUserId,
+        lastSeen: new Date().toISOString(),
+      });
+      console.log(`User ${disconnectedUserId} is offline`);
+    }
   });
 });
 
